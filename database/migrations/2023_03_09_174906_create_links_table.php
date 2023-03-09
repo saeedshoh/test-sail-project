6cbb2f1cf05b04f7ad794d05398ff3bf95876c35
Saeedshoh <52253289+Saeedshoh@users.noreply.github.com>
2023-03-09 23:13:11 +0500

    add models, migrations and factories for links table

---
+++ database/migrations/2023_03_09_174906_create_links_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->id();
+            $table->string('url');
+            $table->string('key')->unique();
+            $table->integer('counter')->default(0);
+            $table->boolean('status')->default(1)->comment('1 = active, 0 = inactive');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('links');
+    }
+};
